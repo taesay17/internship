ac176e55bbb422afbc223fa7dc5e5f341fa47f11
taesay <aikan.abdrashitova@alatoo.edu.kg>
2025-05-27 13:18:48 +0600

    feat: create main Spring Boot class

---
+++ src/main/java/com/example/FreshFarm/Delivery/FreshFarmDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.example.FreshFarm.Delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshFarmDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshFarmDeliveryApplication.class, args);
+	}
+
+}
